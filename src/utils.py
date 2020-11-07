e901625b8a4fac0a59668d116fce3576600304a0
Abhinav Gupta <abhinavgupta2004@gmail.com>
2020-11-07 15:06:57 -0500

    Debugging

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,9 @@ from src.dataset import Multimodal_Datasets
 def get_data(args, dataset, split='train'):
     alignment = 'a' if args.aligned else 'na'
     data_path = os.path.join(args.data_path, dataset) + f'_{split}_{alignment}.dt'
+
+    print(f"data_path: {data_path}")
+
     if not os.path.exists(data_path):
         print(f"  - Creating new {split} data")
         data = Multimodal_Datasets(args.data_path, dataset, split, args.aligned)
